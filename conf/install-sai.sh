e8ffcfbce3c464800ec060abb23683802b1999f5
goodboy23 <31239628+goodboy23@users.noreply.github.com>
2018-04-08 20:39:18 +0800

    Update install-sai.sh

--- conf/install-sai.sh
+++ conf/install-sai.sh
@@ -3,13 +3,13 @@
 
 yum -y install git
 rpm -q git
-if [ $? -eq 0 ];then
+if [ $? -ne 0 ];then
     echo "Git can not be installed, please install manually"
     exit 2
 fi
 
 git clone https://github.com/goodboy23/shell-auto-install.git
-if [ -d shell-auto-install ];then
+if [ ! -d shell-auto-install ];then
     echo "Download failed, please re-execute the script"
     exit 3
 fi
